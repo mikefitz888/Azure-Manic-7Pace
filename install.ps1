ba7631d1a63e67e58d69322b6c256119fdc129ba
noodlemctwoodle <15075388+noodlemctwoodle@users.noreply.github.com>
2019-03-27 12:05:15 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,4 +2,4 @@ $sourceDir = ".\manictime-client-plugin-example\installable-plugin\Release\Plugi
 $targetDir = $Env:systemdrive + "\Users\" + $Env:username + "\AppData\Local\Finkit\ManicTime\Plugins\"
 Copy-Item -Path $sourceDir -Recurse -Destination $targetDir -Container -Force
 Write-Host "Installed. Press any key to continue ..."
-cmd /c pause | out-null
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
