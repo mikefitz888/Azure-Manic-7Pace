2d05b135f09cdc51e5930dbf6bc1ea1fb06fa9af
Michael Fitzpatrick <michael.fitzpatrick@chorus.co>
2019-03-27 09:45:35 +0000

    Included Release Files

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
-$sourceDir = ".\manictime-client-plugin-example\installable-plugin\Release\Plugins\Packages"
-$targetDir = $Env:systemdrive + "\Users\" + $Env:username + "\AppData\Local\Finkit\ManicTime\Plugins\Packages"
+$sourceDir = ".\manictime-client-plugin-example\installable-plugin\Release\Plugins\Packages\"
+$targetDir = $Env:systemdrive + "\Users\" + $Env:username + "\AppData\Local\Finkit\ManicTime\Plugins\"
 Copy-Item -Path $sourceDir -Recurse -Destination $targetDir -Container -Force
 Write-Host "Installed. Press any key to continue ..."
 cmd /c pause | out-null
